5dec4d1f963ec14717ddecd1267a2177d813601c
Niranjan2054 <ckim6189@gmail.com>
2019-12-04 23:19:06 +0545

    Added Game View to Game Activity

--- app/src/main/java/com/niranjan2054/spacejet/activities/GameActivity.java
+++ app/src/main/java/com/niranjan2054/spacejet/activities/GameActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.niranjan2054.spacejet.R;
+import com.niranjan2054.spacejet.modal.GameView;
 
 public class GameActivity extends AppCompatActivity {
-
+private GameView gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+//        setContentView(R.layout.activity_game);
+
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
     }
 }
