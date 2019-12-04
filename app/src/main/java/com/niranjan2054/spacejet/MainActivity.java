e869d7bcfb327330ab176d72c9ef70e1a4e2bf16
Niranjan2054 <ckim6189@gmail.com>
2019-12-04 22:58:05 +0545

    Orientation set Landscape

--- app/src/main/java/com/niranjan2054/spacejet/MainActivity.java
+++ app/src/main/java/com/niranjan2054/spacejet/MainActivity.java
@@ -2,13 +2,27 @@ package com.niranjan2054.spacejet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+protected ImageView buttonPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        buttonPlay = findViewById(R.id.playButton);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Play Now", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
