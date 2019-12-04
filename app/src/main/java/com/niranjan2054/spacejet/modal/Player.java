ee152685b5c9e9136c00bf90413ae69b51270d8e
Niranjan2054 <ckim6189@gmail.com>
2019-12-04 23:51:08 +0545

    Boostr added to Space jet

--- app/src/main/java/com/niranjan2054/spacejet/modal/Player.java
+++ app/src/main/java/com/niranjan2054/spacejet/modal/Player.java
@@ -11,11 +11,26 @@ public class Player {
     //Bitmap to get character from image
     private Bitmap bitmap;
 
+    // boolean bariable to track the ship is boosting or not
+    private Boolean boosting;
+
+    //Gravity value to add gravity effect on the ship
+    private final int GRAVITY = -10;
+
+    //Min speed
+    private final int MIN_SPEED = 1;
+    //Max speed
+    private final int MAX_SPEED = 20;
+
     //position of player
     private int x;
     private int y;
     //Motion Speed of player
     private int speed = 0;
+
+    //controlling y Coordinate
+    private int maxY;
+    private int minY;
 
     public Player(Context context){
        x = 75;
@@ -23,6 +38,7 @@ public class Player {
        speed =1;
        //Getting bitmap from drawable resources
        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.player);
+       boosting = false;
     }
 
     public Bitmap getBitmap() {
@@ -42,7 +58,25 @@ public class Player {
     }
 
     public void update(){
-        //Updating the coordinate of x
-        x++;
+        if (boosting){
+            speed+=2;
+        }else{
+            speed-=5;
+        }
+        if (speed>MAX_SPEED){
+            speed = MAX_SPEED;
+        }else if (speed<MIN_SPEED){
+            speed = MIN_SPEED;
+        }
+
+        //moving the ship down
+        y-= speed + GRAVITY;
+
+        //Controlling the ship so that it doesnot go out of the screen
+//        if(y<minY){
+//            y = minY;
+//        }else if(y>maxY){
+//            y = maxY;
+//        }
     }
 }
